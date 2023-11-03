8bf9d40bc6a2595e9330416b4e06b5465ccba2cb
Risa <risa.luthor@gmail.com>
2023-11-02 20:56:08 -0500

    Add files via upload
    Pokemon Directory

---
+++ Pokemon_Dictionary.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 19:01:32 2022
+
+@author: risaluthor
+"""
+pokemon_Dictionary = {'Abra':'Kadabra', 'Aipom':'Ambipom', 'Alakzam':'Mega Alakzam', 
+                      'Arbok':'Unknown', 'Arcanine':'Arceus', 'Articuno':'Mega Articuno', 'Beedrill':'Mega Beedrill',
+                      'Bellsprout':'Weepinbell', 'Blastoise':'Mega Blastoise', 'Bulbasaur':'Ivysaur', 'butterfree':'Mega Butterfree', 
+                      'Caterpie':'Metapod', 'Charizard':'Mega Charizard X', 'Charmeleon':'Charizard', 'Cefable': 'Mega Clefable', 
+                      'Clefairy':'Clefable', 'Cloyster':'Mega Cloyster', 'Cubone':'Marowak', 'Dewgong':'Mega Dewgong Q', 
+                      'Diglett':'Dugtrio', 'Ditto':'Transforms into all pokemon', 'Doduo':'Dodrio', 'Dragonair':'Dragonite', 
+                      'Dratini':'Dragonair', 'Drowzee':'Hypno', 'Eeevee':'one of several choices depending on stone Flareon', 
+                      'Ekans':'Arbok', 'Electrabuzz':'Electivire', 'Electrode':'Mega Electrode', 'Exeggcute':'Exeggutor', 
+                      'Farfetch':'Sir Farfetch', 'Fearow':'Mega Fearow', 'Gastly':'Gengar', 'Geodude':'Golem', 'Gloom':'Vileplume', 
+                      'Golbat':'Crobat', 'Goldeen':'Seaking', 'Golduck':'Mega Golduck', 'Golem':'Graveler', 'Grimer':'Muk', 
+                      'Haunter':'Mega Haunter', 'Hitmonchan':'N/A', 'Hitmonlee':'N/A','Horsea':'Seadra','Jynx':'Mega Jynx Q',
+                      'Kabuto':'Kabutops','Kakuna':'Beedrill','Kangaskhan':'N/A','Kingler':'Gigantamax Kingler','Koffing':'Weezing',
+                      'Krabby':'Kingler','Lapras':'Mega Lapras','Lickitung':'Lickilicky','Machop':'Machoke','Machamp':'Mega Machamp'
+                      ,'Magikarp':'Gyarados','Magmar':'Magmortar','Magnemite':'Magneton','Mankey':'Primate','Meowth':'Persian',
+                      'Metapod':'Butterfree','Mew':'Mewtwo',}
+
+#for thing in pokemon_Dictionary:
+   # print(thing)
+   
+for key in pokemon_Dictionary:
+       print(key,'-->', pokemon_Dictionary[key])
+             
+    
