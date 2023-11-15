a3d02c87f4cfcf040073d07118444878f76c8e9e
Risa <risa.luthor@gmail.com>
2023-11-15 13:34:18 -0600

    Add files via upload

---
+++ Gradebook.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 13:10:41 2023
+
+@author: luthorcorp
+"""
+
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+subjects = [["Physics", "Calculus", "Poetry", "History"]]
+grades = [[98, 97, 85, 88]]
+
+gradebook = [["Physics", 98], ["Calculus", 97], ["Poetry", 85], ["History", 88]]
+
+print(gradebook)
+
+gradebook.append(["Computer Science", 100])
+
+print(gradebook)
+
+gradebook.append(["Visual arts", 93])
+
+print(gradebook)
+
+gradebook[-1][-1] = 98
+
+print(gradebook)
+
+gradebook[2].remove(85)
+
+gradebook[2].append("Pass")
+
+print(gradebook)
+
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
